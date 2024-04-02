9ad15f2ae6a833a588d947133a2bd0d2e9eb3a8a
srdjanbolozan <45144405+srdjanbolozan@users.noreply.github.com>
2024-04-02 21:21:29 +0200

    Add files via upload

---
+++ A11_2024/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace A11_2024
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.analizaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oAplikacijiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.izlazToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.analizaToolStripMenuItem,
+            this.oAplikacijiToolStripMenuItem,
+            this.izlazToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(893, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // analizaToolStripMenuItem
+            // 
+            this.analizaToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.analizaToolStripMenuItem.Name = "analizaToolStripMenuItem";
+            this.analizaToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.analizaToolStripMenuItem.Text = "Izlaz";
+            // 
+            // oAplikacijiToolStripMenuItem
+            // 
+            this.oAplikacijiToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.oAplikacijiToolStripMenuItem.Name = "oAplikacijiToolStripMenuItem";
+            this.oAplikacijiToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.oAplikacijiToolStripMenuItem.Text = "O aplikaciji";
+            // 
+            // izlazToolStripMenuItem
+            // 
+            this.izlazToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.izlazToolStripMenuItem.Name = "izlazToolStripMenuItem";
+            this.izlazToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.izlazToolStripMenuItem.Text = "Analiza";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(893, 496);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem analizaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oAplikacijiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem izlazToolStripMenuItem;
+    }
+}
+
